1b77ca1d724a91b5ac3be557c9710b158a8c064b
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2022-04-06 17:37:38 -0700

    Added Deploy Prep

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from distutils.log import debug
 from event_app import app
+import os
+
+port = int(os.environ.get('PORT', 33507))
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=8000)
+  app.run(debug=True, host='0.0.0.0', port=port)
